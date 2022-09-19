34f4e7fa8e45b754f3ff4fdbdce01a015037e9c0
Anish Pandit <anishpandit0703@gmail.com>
2022-09-20 00:23:07 +0530

    removed signup

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,6 @@ import { BlitzPage } from "@blitzjs/next"
 import { useRouter } from "next/router"
 import Layout from "app/core/layouts/Layout"
 import { LoginForm } from "app/auth/components/LoginForm"
-import { SignupForm } from "app/auth/components/SignupForm"
 
 const Login: BlitzPage = () => {
   const router = useRouter()
@@ -11,12 +10,6 @@ const Login: BlitzPage = () => {
     <div>
       <LoginForm
         onSuccess={async (_user) => {
-          const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
-          await router.push(next)
-        }}
-      />
-      <SignupForm
-        onSuccess={async () => {
           const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
           await router.push(next)
         }}
